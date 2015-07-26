b589657c1870d6289b17249e2699b2ec5b1a2d39
dcamposliz <dcampos.liz@gmail.com>
2015-07-26 12:13:38 -0700

    updated ex14

--- ex14.py
+++ ex14.py
@@ -3,17 +3,26 @@
 
 from sys import argv
 
+
+#we assign three parameters to "argv"
 script, user_name, age = argv
+#and assign '> ' to the variable prompt
 prompt = '> '
 
+# then print a variety of shit that takes the 
+# arguments we pass to the terminal when executing
+# the script
 print "Hi %s, I'm the %s script." % (user_name, script)
 print "I'd like to ask you a few questions."
 print "Do you like me %s?" % user_name
+# we assign the input from the user to the variable likes
 likes = raw_input(prompt)
 
+# we assign the input from the user to the variable lives
 print "Where do you live %s?" % user_name
 lives = raw_input(prompt)
 
+# we assign the input from the user to the variable computer
 print "What kind of computer do you have?"
 computer = raw_input(prompt)
 

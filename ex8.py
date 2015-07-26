84969982f4c9330992924ed15562f884c517d06b
dcamposliz <dcampos.liz@gmail.com>
2015-07-26 09:54:05 -0700

    pushing exercises 8, 9, 10, 11, 12

--- ex8.py
+++ ex8.py
@@ -1,18 +1,12 @@
-# Here's some new strange stuff, remember to type it exactly the way it is:
+formatter = "%r %r %r %r"
 
-days = "Mon Tue Wed Thu Fri Sat Sun"
-months = "Jan\nFeb\nMar\nApr\nMay\nJun\nAug"
-
-print "Here are the days: ", days
-print "Here are the months: ", months
-
-print """
-
-There's something going on here.
-With the three double-quotes.
-We'll be able to type as much as we like.
-Even 4 lines if we want, or 5, or 6.
-
-"""
-
-# seems that no mistakes were made in this one :)
+print formatter % (1, 2, 3, 4)
+print formatter % ("one", "two", "three", "four")
+print formatter % (True, False, False, True)
+print formatter % (formatter, formatter, formatter, formatter)
+print formatter % (
+		"I had this thing.",
+		"That you could type up right.",
+		"But it didn't sing.",
+		"So I said goodnight.",
+	)
---
+++ ex9.py
@@ -0,0 +1,18 @@
+# Here's some new strange stuff, remember to type it exactly the way it is:
+
+days = "Mon Tue Wed Thu Fri Sat Sun"
+months = "Jan\nFeb\nMar\nApr\nMay\nJun\nAug"
+
+print "Here are the days: ", days
+print "Here are the months: ", months
+
+print """
+
+There's something going on here.
+With the three double-quotes.
+We'll be able to type as much as we like.
+Even 4 lines if we want, or 5, or 6.
+
+"""
+
+# seems that no mistakes were made in this one :)

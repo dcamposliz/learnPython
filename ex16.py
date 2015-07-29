fd0a3baa058af1c3f524816bbb8d3a130ed2f77b
dcamposliz <dcampos.liz@gmail.com>
2015-07-29 14:06:05 -0700

    updated 16, added 22

--- ex16.py
+++ ex16.py
@@ -1,19 +1,33 @@
-
 #again, we are plugging in argv from the module sys
 from sys import argv
 
+#again, assigning 'value' argv to variables script and filename
 script, filename = argv
 
+#printing stuff
 print "We're going to erase %r." % filename
 print "If you don't want that, hit CTRL-C (^C)."
 print "If you do want that, hit RETURN."
 
+#running method raw_input, which asks user for input given the questions asked above
 raw_input("?")
 
+#printing some shit
 print "Opening the file..."
+#defining variable 'target' and assigning method 'open', which takes parameters 'filename' and 'w'
+#we previously defined 'filename' to be a function of user input
+#i have no idea what 'w' is, however :(
+#actually, w stands for the mode in which the file is opened, which is 'write'
+#analogously, r stands for 'read', and a stands for 'append'
+#does this mean that open always uses two parameters?
+#well, according to the python documentation, the following is the standard format for 'calling' the open method
+	# open(name[,mode[,buffering]])
+		# the buffer argument option specifies the file's buffer size - which is another google away, but im gonna stay focused for now
 target = open(filename, 'w')
 
+#we print!
 print "Truncating the file. Goodbye!"
+#we run our 'function' target, and 
 target.truncate()
 
 print "Now I'm going to ask you for three lines."

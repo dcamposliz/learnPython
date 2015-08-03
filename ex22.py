3ebe26aba2d1ad4c1f3aad2bee3bdacb2ffbf00a
dcamposliz <dcampos.liz@gmail.com>
2015-08-02 21:40:59 -0700

    modified ex22 and added ex23

---
+++ ex22.py
@@ -0,0 +1,23 @@
+print "This is how we print a string"
+
+#this is how we comment stuff
+
+print "We can print strings, math, booleans:", 5 + 5, True
+
+#we can declare variables, and print their values
+
+cars = 100
+print "There are", cars, "cars" 
+
+#we can also print 'stuff' within strings
+print "There are %r cars" % (cars)
+print "There are %s cars" % (cars)
+print "There are %d cars" % (cars)
+
+hairSalon = "lots of hair cutting"
+
+print "There is %r in hair salons" % (hairSalon)
+print "There is %s in hair salons" % (hairSalon)
+# print "There is %d in hair salons" % (hairSalon)
+	# we commented line 21 because it does not make sense to the interpreter
+
---
+++ ex23.py
@@ -0,0 +1,3 @@
+#started looking at the code from youtube-dl
+
+#link is https://github.com/rg3/youtube-dl

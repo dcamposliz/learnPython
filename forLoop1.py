232a0b282eb3f8dd41ca415c6532061ba5697ae4
dcamposliz <dcampos.liz@gmail.com>
2015-08-06 19:23:17 -0700

    added loops1.py

--- forLoop1.py
+++
@@ -1,7 +0,0 @@
-
-# Source:
-# https://www.youtube.com/watch?v=xtXexPSfcZg
-
-exampleList = [1,3,454,6,56,56,56,5,65,34]
-
-for eachNumber in exampleList:
---
+++ loops1.py
@@ -0,0 +1,20 @@
+
+# Source:
+# https://www.youtube.com/watch?v=xtXexPSfcZg
+
+# we are declaring a list to be an array
+# of integers, 5 integers, to be specific
+exampleList = [1,3,454,6,56]
+
+# then we declare a for loop, where we iterate
+# through the list we just created :)
+# this actually executes when we run the script
+
+print "We are running a for loop on a list"
+for eachNumber in exampleList:
+	print(eachNumber)
+
+print "We are running a while loop on a range"
+for x in range(1,11):
+	print x
+

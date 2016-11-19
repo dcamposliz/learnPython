568a97836a4f30c751053f192d4e88caa2459662
dcamposliz <dcampos.liz@gmail.com>
2016-11-18 20:57:35 -0800

    drawing things with python

--- random_python/turtleDraw.py
+++ random_python/turtleDraw.py
@@ -1,14 +1,22 @@
-# ON TERMINAL:
+###########################################
 #
-#	python2
+#	Drawing with Turtle
+#	Author: David Campos
 #
-#	import turtleDraw as td
+###########################################
 #
-#	td.square()
-#	td.triangle()
-#	td.circle()
-#	td.spiral()
-#	td.squareSpiral()
+#	Execute on terminal:
+#
+#		python2
+#
+#		import turtleDraw as td
+#
+#		td.square()
+#		td.triangle()
+#		td.circle()
+#		td.spiral()
+#		td.squareSpiral()
+#		td.circleSpiral()
 
 import turtle as tl
 import time as tm
@@ -16,9 +24,9 @@ import time as tm
 def square():
 	for x in range(0,3):
 		tl.forward(100)
-		tm.sleep(.5)
+		tm.sleep(.1)
 		tl.left(90)
-		tm.sleep(.5)
+		tm.sleep(.1)
 	tl.forward(100)
 
 def triangle():
@@ -28,16 +36,12 @@ def triangle():
 		tl.left(120)
 		tm.sleep(.5)
 	tl.forward(100)
-	tl.exitonclick()
 
 def circle():
 	for x in range(0,360):
 		tl.forward(2)
-		tm.sleep(.0001)
 		tl.left(1)
-		tm.sleep(.0001)
 	tl.forward(2)
-	tl.exitonclick()
 
 def spiral():
 	degInc = 1
@@ -46,7 +50,6 @@ def spiral():
 		tl.left(degInc)
 		degInc = degInc + .001
 	tl.forward(2)
-	tl.exitonclick()
 
 def squareSpiral():
 	tl.left(30)
@@ -55,10 +58,16 @@ def squareSpiral():
 		square()
 		tl.left(120)
 
+def circleSpiral():
+	for c in range(0,12):
+		circle()
+		tl.left(30)
+
 print "\n\n RUN THIS PROGRAM:"
 print "\n\tturtleDraw.square()"
 print "\n\tturtleDraw.triangle()"
 print "\n\tturtleDraw.circle()"
 print "\n\tturtleDraw.spiral()"
 print "\n\tturtleDraw.squareSpiral()"
+print "\n\tturtleDraw.circleSpiral()"
 

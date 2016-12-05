b957fe014d80150b2c1bba8fc1d4c4b419023d0f
dcamposliz <dcampos.liz@gmail.com>
2016-12-04 16:34:18 -0800

    removed virtualenv pip stuff, which can be recovered by running'pip install -r requirements.txt'

--- frameworks/djangoLearn/project1_container/venv/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/home/dc/myProjects/learn/python/frameworks/djangoLearn/project1_container/venv/bin/python3
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
